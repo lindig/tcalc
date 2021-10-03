1863ee45f303be69dbded37f011d84e8b489f44b
Christian Lindig <lindig@gmail.com>
2021-10-03 15:15:13 +0100

    Format output as hour:min:sec
    Signed-off-by: Christian Lindig <lindig@gmail.com>

--- bin/main.ml
+++ bin/main.ml
@@ -1,10 +1,23 @@
+let ( // ) x y =
+  let div = x /. y |> Float.trunc in
+  let rem = x -. (div *. y) in
+  (div, rem)
+
+let hms seconds =
+  let h, s = seconds // 3600.0 in
+  let m, s = s // 60.0 in
+  (h, m, s)
+
+let result seconds =
+  let h, m, s = hms seconds in
+  Printf.printf "%5.2f (%02.0f:%02.0f:%05.2f)\n%!" seconds h m s
+
 let main () =
   try
     let lexbuf = Lexing.from_channel stdin in
     while true do
-      let result = Tcalc.Parser.main Tcalc.Lexer.token lexbuf in
-      Printf.printf "%5.2f\n" result;
-      flush stdout
+      let seconds = Tcalc.Parser.main Tcalc.Lexer.token lexbuf in
+      result seconds
     done
   with Tcalc.Lexer.EOF -> exit 0
 
